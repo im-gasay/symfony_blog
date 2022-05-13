8a212fedee852ad2ccdf4f1bccdb867f55c4fa37
Kelle Konstantin <kelle.konstantin@gmail.com>
2022-05-13 18:19:48 +0700

    Add support doctrine. Add post entity. Add post controller

---
+++ src/Entity/Post.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $title;
+
+    #[ORM\Column(type: 'text')]
+    private $content;
+
+    #[ORM\Column(type: 'string')]
+    private $image_url;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $post_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image_url): self
+    {
+        $this->image_url = $image_url;
+
+        return $this;
+    }
+
+
+    public function getPostAt(): ?\DateTimeImmutable
+    {
+        return $this->post_at;
+    }
+
+    public function setPostAt(\DateTimeImmutable $post_at): self
+    {
+        $this->post_at = $post_at;
+
+        return $this;
+    }
+}
