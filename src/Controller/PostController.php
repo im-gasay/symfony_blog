bb4c625fbe1cb055f4f0d1f8710e846a9ba2128c
Kelle Konstantin <kelle.konstantin@gmail.com>
2022-05-15 19:37:14 +0700

    Add posts pages. Add post 404 page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+
+
+    #[Route('/', name: 'app_posts')]
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PostController.php',
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/posts/{id}', name: 'app_post')]
+    public function post(int $id, PostRepository $postRepository)
+    {
+        $post = $postRepository->find($id);
+
+        if (is_null($post)) {
+            return $this->render('post/404.html.twig');
+        }
+
+        return $this->render('post/page.html.twig', [
+            'post' => $post
         ]);
     }
 }
